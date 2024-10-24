3161353caebfb0ccac6b760dc4af8b39494c2421
Mateus Martins S. <108599241+MateusMaartinS@users.noreply.github.com>
2024-10-24 17:08:22 -0300

    Update procurar_img.py

--- procurar_img.py
+++ procurar_img.py
@@ -3,9 +3,10 @@ import time
 import random
 import mousekey
 
-mkey = mousekey.MouseKey()
 
-def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60, horizontal=0, vertical=0, acao='clicar', clicks=1):
+
+def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60, horizontal=0, vertical=0, acao='clicar', clicks=1, ocorrencia=1):
+    mkey = mousekey.MouseKey()
     
     def click(x, y):
         pyautogui.click(x, y)
@@ -16,7 +17,7 @@ def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60
     def coordenada(x, y):
         print(f'Coordenadas da imagem: ({x}, {y})')
 
-    def move_mouse(x,y,variationx=(-5, 5),variationy=(-5, 5),up_down=(0.2, 0.3),min_variation=-10,max_variation=10,use_every=4,sleeptime=(0.009, 0.019),linear=90,):
+    def move_mouse(x, y, variationx=(-5, 5), variationy=(-5, 5), up_down=(0.2, 0.3), min_variation=-10, max_variation=10, use_every=4, sleeptime=(0.009, 0.019), linear=90):
         mkey.left_click_xy_natural(
             int(x) - random.randint(*variationx),
             int(y) - random.randint(*variationy),
@@ -28,36 +29,46 @@ def procurar_imagem(nome_arquivo, confidence=0.8, region=None, max_tentativas=60
             print_coords=True,
             percent=linear,
         )
-    
+
     acoes_validas = ['clicar', 'mover_clicar']
 
     if acao not in acoes_validas:
         raise ValueError(f"Ação inválida: '{acao}'. Escolha entre {acoes_validas}.")
 
     tentativas = 0
-
     while tentativas < max_tentativas:
         tentativas += 1
         try:
-            img = pyautogui.locateCenterOnScreen(nome_arquivo, confidence=confidence, region=region)
-            if img:
-                x, y = img
-                x += horizontal
-                y += vertical
-                #coordenada(x, y)
-                if acao == 'clicar':
-                    if clicks == 1:
-                        click(x, y)
-                    else:
-                        doubleClick(x, y)
-                elif acao == 'mover_clicar':
-                    move_mouse(x, y)
-                return True
+            imag = list(pyautogui.locateAllOnScreen(nome_arquivo, confidence=confidence, region=region))
+            
+            if imag:
+                if len(imag) >= ocorrencia:
+                    img = imag[ocorrencia - 1]  
+                    x, y = pyautogui.center(img) 
+                    x += horizontal
+                    y += vertical
+
+                    if acao == 'clicar':
+                        match clicks:
+                            case 0:
+                                coordenada(x, y)
+                            case 1:
+                                click(x, y)
+                            case 2:
+                                doubleClick(x, y)
+
+                    elif acao == 'mover_clicar':
+                        move_mouse(x, y)
+                    return True
+                else:
+                    print(f'A ocorrência {ocorrencia} não foi encontrada.')
+                    return False
+
         except pyautogui.ImageNotFoundException:
             pass
         time.sleep(1)
 
-    print(f'Imagem não encontrada após {max_tentativas} segundos.')
+    print(f'Imagem não encontrada após {max_tentativas} tentativas.')
     return False
 
 procurar_imagem('testeCaptcha.png', acao='mover_clicar')
